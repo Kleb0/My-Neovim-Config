15c03c9b6c7e69736e9abacfa1f42c333efaa8ae
Klebo <klebomail@something.com>
2026-07-31 17:07:07 +0200

    31-07-2026

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,6 +9,12 @@ end, { desc = "Select all" })
 vim.keymap.set({ "n", "v" }, "<C-c>", '"+y', { desc = "Copy to clipboard" })
 
 vim.keymap.set("n", "<C-v>", '"+p', { desc = "Paste from clipboard" })
-vim.keymap.set("i", "<C-v>", '<C-r>+', { desc = "Paste from clipboard" })
+vim.keymap.set("i", "<C-v>", "<C-r>+", { desc = "Paste from clipboard" })
 
 vim.keymap.set({ "n", "i" }, "<C-z>", "<Cmd>undo<CR>", { desc = "Undo" })
+
+vim.keymap.set("n", "<C-f>", function()
+  require("snacks").picker.lines()
+end, {
+  desc = "Find in current file",
+})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.number = true
+vim.opt.relativenumber = false
+
 vim.api.nvim_set_hl(0, "SnacksPickerDir", {
   fg = "#ff9e64",
   bg = "#2a2a2a",
